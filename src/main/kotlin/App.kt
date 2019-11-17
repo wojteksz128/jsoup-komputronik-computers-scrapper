0ef0a73bfaf17980164afe25a883bb8ac9332d3e
Adrian.Rozalski <adrianroz00@gmail.com>
2019-11-17 21:26:07 +0100

    Added fetching productVariantGroup

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,12 +1,27 @@
 import org.jsoup.Jsoup
+import org.jsoup.select.Elements
 
 fun main() {
     val doc = Jsoup.connect("https://www.komputronik.pl/category/5801/komputery-pc.html").get()
-    println(doc.title())
+    println(doc.title()+"\n")
     val newsHeadlines = doc.select("#products-list > div .pe2-head a.blank-link")
+
+    fetchPcList(newsHeadlines)
+}
+
+fun fetchPcList(newsHeadlines:Elements)
+{
     for (headline in newsHeadlines) {
         if (!headline.childNodes().first().toString().startsWith(" {{")) {
-            println("${headline.childNodes().first()}\n\t${headline.absUrl("href")}")
+            if (headline.absUrl("href").contains("productVariantGroup")){
+                val doc2 = Jsoup.connect(headline.absUrl("href")).get()
+                val newsHeadlines2 = doc2.select("#products-list > div .pe2-head a.blank-link")
+
+                fetchPcList(newsHeadlines2)
+            }
+            else {
+                println("${headline.childNodes().first()}\n\t${headline.absUrl("href")}")
+            }
         }
     }
 }
